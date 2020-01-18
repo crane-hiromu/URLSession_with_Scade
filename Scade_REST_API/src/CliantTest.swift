f36e96d33742fe5b708d35de55a9fafa8dc2c2fb
h.crane <h.crane.t@gmail.com>
2020-01-18 16:21:15 +0900

    fix indent

--- Scade_REST_API/src/CliantTest.swift
+++ Scade_REST_API/src/CliantTest.swift
@@ -1,9 +1,9 @@
 import ScadeKit
 
 class CliantTest {
-		
-	func testDecode() {
-		let data = """
+    
+    func testDecode() {
+        let data = """
 			{
 			"model": "iPhone X",
 			"displaySize": 5.8,
@@ -11,44 +11,45 @@ class CliantTest {
 			"biometricsAuth": "Face ID"
 			}
 		"""
-		.data(using: .utf8)!
-		
-		let device = try? JSONDecoder().decode(Device.self, from: data)
-		let encoder = JSONEncoder()
-		encoder.outputFormatting = .prettyPrinted
-		
-		do {
-			let encoded = try encoder.encode(device)
-			print("------do", String(data: encoded, encoding: .utf8)!)	
-		} catch let error {
-			print("------error", error)
-		}
-	}
-	
-	func testCall() {
-       let url = URL(string: "https://api.droidkaigi.jp/2020/speakers/")!
-       let session = URLSession.shared
-  
-       var request = URLRequest(url: url)
-	   request.httpMethod = "GET"
-	   request.addValue("application/json;charset=utf-8", forHTTPHeaderField: "Content-Type")
-	   
-	   let task = session.dataTask(with:request,completionHandler: { data, response, error in
-		   print(data, response, error.debugDescription, error?.localizedDescription)
-		   
-		   if let d = data {
-		   	 let str = String(data: d, encoding: .utf8)
-		   	 print("----data", data, d)
-	   	   }
-	   })
-	   task.resume()
-	   
-	}
+            .data(using: .utf8)!
+        
+        
+        let device = try? JSONDecoder().decode(Device.self, from: data)
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        
+        do {
+            let encoded = try encoder.encode(device)
+            print("------do", String(data: encoded, encoding: .utf8)!)
+        } catch let error {
+            print("------error", error)
+        }
+    }
+    
+    func testCall() {
+        let url = URL(string: "https://api.droidkaigi.jp/2020/speakers/")!
+        let session = URLSession.shared
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("application/json;charset=utf-8", forHTTPHeaderField: "Content-Type")
+        
+        let task = session.dataTask(with:request,completionHandler: { data, response, error in
+            print(data, response, error.debugDescription, error?.localizedDescription)
+            
+            if let d = data {
+                let str = String(data: d, encoding: .utf8)
+                print("----data", data, d)
+            }
+        })
+        task.resume()
+        
+    }
 }
 
 struct Device: Codable {
-	var model: String
-	var displaySize: Float
-	var capacities: [Int]
-	var biometricsAuth: String?
+    var model: String
+    var displaySize: Float
+    var capacities: [Int]
+    var biometricsAuth: String?
 }
